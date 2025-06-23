8154545d1041f4a35d50ac627f0bafdf7cfdb5ef
telim0120 <241002@std.hi-joho.ac.jp>
2025-06-23 11:35:50 +0900

    Update Character.java

--- src/Character.java
+++ src/Character.java
@@ -7,8 +7,6 @@ public abstract class Character implements Creature {
         this.hp = hp;
     }
 
-    public abstract void attack(Character target);
-
     public final boolean isAlive() {
         return this.hp > 0;
     }
